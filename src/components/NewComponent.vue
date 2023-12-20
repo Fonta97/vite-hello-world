c641f29b25fc9bccd96b9ed2e54fbdca6d529045
Fonta97 <fontana.alessandro97@gmail.com>
2023-12-20 20:29:50 +0100

    Hello Vite in Vue GABIBBO EDISCION

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,24 @@
+<script>
+
+
+export default {
+    name: "NewComponent"
+}
+
+</script>
+
+<template>
+    <h1 class="color">Hello Vite in Vue! Stai attento al GABIBBO!</h1>
+    <h2 class="papere">PAPEREEEEE</h2>
+    <img src="https://i.redd.it/qhqsiy2j7l191.png" alt="SPLASH">
+</template>
+
+<style scoped>
+.color {
+    color: #888;
+}
+
+.papere {
+    color: rgb(239, 183, 79);
+}
+</style>
